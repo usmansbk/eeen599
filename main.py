fdfbe6c1cedc29c8067b51ae2f9e47fabdb8124e
uxximan <usmansbk@gmail.com>
2017-07-28 06:37:07 +0100

    added GNU GPL

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python
+# Copyright 2017 Babakolo Usman Suleiman
+# This program is distributed under the terms of the GNU
+# General Public License (GPL).
 """
 App main entry point
 """
